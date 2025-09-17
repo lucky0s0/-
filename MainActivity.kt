f3518d6d3211ccec858a8357c32d29f4c270f6e5
lucky0s0 <guswns02211@naver.com>
2025-09-17 14:19:33 +0900

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.w03
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.w03.ui.theme.ChoiTheme
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ChoiTheme {
+                HomeScreen()
+            }
+        }
+        }
+    }
+
+@Composable()
+fun HomeScreen(){
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Text("eapa",
+            style = MaterialTheme.typography.headlineMedium)
+        Text("winter",
+            style = MaterialTheme.typography.headlineMedium)
+        Image(
+            painter = painterResource(id = R.drawable.img),
+            contentDescription = "Jetpack Compose 로고",
+            modifier = Modifier
+                .size(400.dp)
+                .padding(5.dp)
+        )
+        Text("LEGEND",
+                style = MaterialTheme.typography.headlineMedium)
+    }
+
+
+}
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview(){
+    HomeScreen()
+}
